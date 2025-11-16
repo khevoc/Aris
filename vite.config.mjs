af842a4cff9843d6cf3c0143bb29739299bda07a
khevoc <121624825+khevoc@users.noreply.github.com>
2025-11-15 20:09:11 -0500

    Change server port from 5176 to 5174

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5176,
+    port: 5174,
     proxy: {
       '/api': { target: 'http://localhost:8080', changeOrigin: true }
-    }
+    },
+    build: {
+    outDir: 'dist',
+  },
+  resolve: { alias: { '@': '/src' } }
   }
 })
