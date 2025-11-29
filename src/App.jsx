3190a96e9a1b7e1fe44a16befb8fd936edb8e0bc
khevoc <121624825+khevoc@users.noreply.github.com>
2025-11-28 22:00:07 -0500

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import Gallery from './components/Gallery'
 import CrocodileExperience from "./pages/CrocodileExperience"
 import NightVision from "./components/CatScene"
-import Demo from "./components/Mockup"
 import './styles/App.css'
 
 export default function App() {
@@ -14,7 +13,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Gallery />} />
         <Route path="/croco-experience" element={<CrocodileExperience />} />
-        <Route path="/mockup" element={<Demo />} />
         <Route path="/night-vision" element={<NightVision />} />
       </Routes>
     </BrowserRouter>
